af19d73a8894a53563c20750c67beb979f03aa7b
Lukas Aurio <lukeaurio@proton.me>
2026-04-08 23:13:08 -0400

    feat(helm): refactor helm releases structure and update variable definitions

--- tofu/deployment/helm.tfvars
+++ tofu/deployment/helm.tfvars
@@ -2,14 +2,14 @@
 # This file contains the variables for the helm module. These variables are used to deploy helm charts to the kubernetes cluster. The values for these variables are defined in the terraform.tfvars file.
 ####
 
-helm_releases = {
-  nginx_ingress = {
+helm_releases = [
+  {
     name          = "nginx"
     repo_url      = "https://charts.bitnami.com/bitnami"
     chart_name    = "nginx"
     version       = "4.0.6"
     namespace     = "nginx-tofutest"
     values_file   = "${path.module}/../helm/nginx/nginx-values.yaml"
-    replica_count = 1
+    replica_count = 0
   }
-}
+]
--- tofu/deployment/helmcharts.tf
+++ tofu/deployment/helmcharts.tf
@@ -3,15 +3,12 @@
 module "helm" {
   source   = "../modules/helm"
   for_each = { for k, v in var.helm_releases : v.name => v }
-
-  kubernetes_cluster_name = google_container_cluster.default.name
-  project_id              = var.project_id
-  region                  = var.region
-  helm_chart_name         = each.value.name
+  helm_chart_name         = each.value.chart_name
   helm_release_name       = each.value.name
   helm_repository_url     = each.value.repo_url
   helm_namespace          = each.value.namespace
   helm_chart_version      = each.value.version
+  helm_values             = each.value.helm_values
   helm_value_file         = each.value.values_file
   replica_count           = each.value.replica_count
 }
