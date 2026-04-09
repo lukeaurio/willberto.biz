23dfb24c0cd0a717c282984e5743228ebd2b2262
Lukas Aurio <lukeaurio@proton.me>
2026-04-08 23:13:18 -0400

    style(helm): format alignment in helm module configuration

--- tofu/deployment/helmcharts.tf
+++ tofu/deployment/helmcharts.tf
@@ -1,14 +1,14 @@
 
 
 module "helm" {
-  source   = "../modules/helm"
-  for_each = { for k, v in var.helm_releases : v.name => v }
-  helm_chart_name         = each.value.chart_name
-  helm_release_name       = each.value.name
-  helm_repository_url     = each.value.repo_url
-  helm_namespace          = each.value.namespace
-  helm_chart_version      = each.value.version
-  helm_values             = each.value.helm_values
-  helm_value_file         = each.value.values_file
-  replica_count           = each.value.replica_count
+  source              = "../modules/helm"
+  for_each            = { for k, v in var.helm_releases : v.name => v }
+  helm_chart_name     = each.value.chart_name
+  helm_release_name   = each.value.name
+  helm_repository_url = each.value.repo_url
+  helm_namespace      = each.value.namespace
+  helm_chart_version  = each.value.version
+  helm_values         = each.value.helm_values
+  helm_value_file     = each.value.values_file
+  replica_count       = each.value.replica_count
 }
