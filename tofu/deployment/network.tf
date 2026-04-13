e7238d2fd2203d71fb4f55a23a5f241193445f0c
Lukas Aurio <lukeaurio@proton.me>
2026-04-13 08:37:51 -0400

    fix(cluster): enable deletion protection and update lifecycle settings

--- tofu/deployment/network.tf
+++ tofu/deployment/network.tf
@@ -1,12 +1,12 @@
 resource "google_compute_network" "default" {
-  name = lower("${var.project_name}")
+  name = var.project_name
 
   auto_create_subnetworks  = false
   enable_ula_internal_ipv6 = true
 }
 
 resource "google_compute_subnetwork" "default" {
-  name = lower("${var.project_name}-sub")
+  name = "${var.project_name}-sub"
 
   ip_cidr_range = "10.0.0.0/24"
   region        = "us-central1"
