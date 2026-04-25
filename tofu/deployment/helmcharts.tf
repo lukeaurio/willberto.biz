5dc1a416a5f258c52ddff83f8c4960ad84d39dc7
Lukas Aurio <lukeaurio@proton.me>
2026-04-25 09:38:31 -0700

    feat: uncomment helm module configuration in helmcharts.tf

--- tofu/deployment/helmcharts.tf
+++ tofu/deployment/helmcharts.tf
@@ -1,20 +1,20 @@
 
-module "helm" {
-  source              = "../modules/helm"
-  for_each            = { for k, v in var.helm_releases : v.name => v if v.disabled != true }
-  helm_chart_name     = each.value.chart_name
-  helm_release_name   = each.value.name
-  helm_repository_url = each.value.repo_url
-  helm_namespace      = each.value.namespace
-  helm_chart_version  = each.value.version
-  helm_values         = each.value.values
-  helm_value_file     = "../../${each.value.values_file}" #Chartpaths are relative to the root of the module
-  replica_count       = each.value.replica_count
-  depends_on          = [resource.google_container_cluster.default]
-}
+#module "helm" {
+#  source              = "../modules/helm"
+#  for_each            = { for k, v in var.helm_releases : v.name => v if v.disabled != true }
+#  helm_chart_name     = each.value.chart_name
+#  helm_release_name   = each.value.name
+#  helm_repository_url = each.value.repo_url
+#  helm_namespace      = each.value.namespace
+#  helm_chart_version  = each.value.version
+#  helm_values         = each.value.values
+#  helm_value_file     = "../../${each.value.values_file}" #Chartpaths are relative to the root of the module
+#  replica_count       = each.value.replica_count
+#  depends_on          = [resource.google_container_cluster.default]
+#}
 
 removed {
-  from = module.helm["nginx"]
+  from = module.helm
   lifecycle {
     destroy = false
   }
