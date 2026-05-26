a30eb430edf91724a9e736e0adc90717b2858558
Lukas Aurio <lukeaurio@proton.me>
2026-05-25 22:50:45 -0400

    fix: remove uneeded secretstore info from helm deploy (intially planned on using )

--- tofu/modules/helm/outputs.tf
+++ tofu/modules/helm/outputs.tf
@@ -17,23 +17,3 @@ output "service_account_namespace" {
   description = "The namespace of the service account created for the Helm release."
   value       = try(kubernetes_service_account.this[0].metadata[0].namespace, null)
 }
-
-output "secret_store_name" {
-  description = "The name of the created SecretStore or ClusterSecretStore."
-  value       = try(kubernetes_manifest.secret_store[0].object.metadata.name, null)
-}
-
-output "secret_store_namespace" {
-  description = "The namespace of the created SecretStore. Null when creating a ClusterSecretStore."
-  value       = try(kubernetes_manifest.secret_store[0].object.metadata.namespace, null)
-}
-
-output "secret_store_kind" {
-  description = "The kind of External Secrets store created by this module."
-  value       = try(kubernetes_manifest.secret_store[0].object.kind, null)
-}
-
-output "secret_store_manifest" {
-  description = "The generated SecretStore or ClusterSecretStore manifest object."
-  value       = try(kubernetes_manifest.secret_store[0].manifest, null)
-}
