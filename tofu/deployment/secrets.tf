20b3ffc4fa6ff7c9097e27570c8a6bc9f4f98fd4
Lukas Aurio <lukeaurio@proton.me>
2026-05-28 08:47:27 -0400

    feat: update for_each to use toset for local.secret_ids in secrets.tf

--- tofu/deployment/secrets.tf
+++ tofu/deployment/secrets.tf
@@ -4,7 +4,7 @@ locals {
 
 
 resource "google_secret_manager_secret" "secrets" {
-  for_each  = nonsensitive(local.secret_ids)
+  for_each  = toset(local.secret_ids)
   project   = var.project_id
   secret_id = var.google_secrets[each.key].secret_id
 
@@ -19,7 +19,7 @@ resource "google_secret_manager_secret" "secrets" {
 }
 
 resource "google_secret_manager_secret_version" "secrets" {
-  for_each = nonsensitive(local.secret_ids)
+  for_each = toset(local.secret_ids)
 
   secret      = google_secret_manager_secret.secrets[each.key].id
   secret_data = templatefile(var.google_secrets[each.key].value, local.variable_overrides)
