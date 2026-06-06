a22815ecbc260dafeb3e89675c7d5d99ef65349f
Lukas Aurio <lukeaurio@proton.me>
2026-06-06 16:39:13 -0400

    fix: :facepalm: Ensure Cloudflare account ID is set in ingress tunnel configuration

--- tofu/deployment/tunnels.tf
+++ tofu/deployment/tunnels.tf
@@ -1,4 +1,5 @@
 resource "cloudflare_zero_trust_tunnel_cloudflared" "ingress_tunnel" {
   name       = "${var.project_name}-tunnel"
+  account_id = var.cloudflare_account_id
   config_src = "cloudflare"
 }
