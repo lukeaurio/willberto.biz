b2fabc6e1ab3d0b37ed9c6171bfe57010008835f
lukeaurio <lukeaurio@proton.me>
2026-06-30 15:57:10 -0400

    fix: correct the ordering of the cf ingress dependent builds (#54)

--- tofu/deployment/helmcharts.tf
+++ tofu/deployment/helmcharts.tf
@@ -34,7 +34,7 @@ module "namespaces" {
 
 module "helm_with_external_secrets" {
   source                 = "../modules/helm"
-  for_each               = { for k, v in var.helm_releases : v.name => v if v.disabled == false && v.uses_external_secret == true }
+  for_each               = { for k, v in var.helm_releases : v.name => v if v.disabled == false && v.uses_external_secret == true && v.uses_cf_ingress == false }
   gcp_project_id         = var.project_id
   gcp_project_name       = local.project_name_sanitized
   helm_chart_name        = each.value.chart_name
@@ -71,5 +71,5 @@ module "helm_with_cf_ingress" {
   create_namespace       = each.value.create_namespace
   accessible_secrets     = each.value.service_account_accessible_secrets
   gcp_roles              = each.value.service_account_gcp_roles
-  depends_on             = [resource.google_container_cluster.autopilot, module.helm_external_secrets, module.namespaces]
+  depends_on             = [resource.google_container_cluster.autopilot, module.helm_with_external_secrets]
 }
