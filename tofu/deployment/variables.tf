a6607e4b312104d78661b05648b3a50e716015da
Lukas Aurio <lukeaurio@proton.me>
2026-07-01 10:46:58 -0400

    fix: update google_secrets variable to specify value type as map(any)

--- tofu/deployment/variables.tf
+++ tofu/deployment/variables.tf
@@ -361,7 +361,7 @@ variable "google_secrets" {
   description = "Optional Google Secret Manager secrets created by Terraform for ExternalSecret testing."
   type = list(object({
     secret_id   = string
-    value       = map()
+    value       = map(any)
     labels      = optional(map(string), {})
     disabled    = optional(bool, false)
     version_env = optional(string, "example")
